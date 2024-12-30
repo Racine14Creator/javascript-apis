bc9bf7c1890c958df60dd6994bd49087800cd126
racine14creator <bisimwagrace596@gmail.com>
2024-12-30 15:30:08 +0200

    Test to the public and private status

--- controllers/test.controller.js
+++ controllers/test.controller.js
@@ -1,56 +1,60 @@
-import Test from "../models/test.js"
+import Test from "../models/test.js";
 
-import connectDB from "../lib/db.mongodb.js"
+import connectDB from "../lib/db.mongodb.js";
 
 export const getTests = async (req, res) => {
-    try {
-        const tests = await Test.find()
+  try {
+    const tests = await Test.find();
 
-        return res.json({message: "All test", data: tests})
-    } catch (error) {
-        console.log(error)
-        return res.json({message: error})
-    }
-}
+    return res.json({ message: "All test", data: tests });
+  } catch (error) {
+    console.log(error);
+    return res.json({ message: error });
+  }
+};
 
 export const getTest = async (req, res) => {
-    const {id} = req.params
+  const { id } = req.params;
 
-    const test = await Test.findById(id);
+  const test = await Test.findById(id);
 
-    if(!test){
-        return res.json({message: "This test is not found"})
-    }
+  if (!test) {
+    return res.json({ message: "This test is not found" });
+  }
 
-    return res.json({test})
-}
+  return res.json({ test });
+};
 
-export const postTest = async (req, res)=>{
-    const {title, description} = await req.body
-    
-    const newTest = new Test({
-        title, description
+export const postTest = async (req, res) => {
+  const { title, description, status /*userId*/ } = await req.body;
+
+  const newTest = new Test({
+    title,
+    description,
+    status,
+    // user: userId,
+  });
+
+  newTest
+    .save()
+    .then((_) => {
+      return res.status(200).json({ message: "success", data: newTest });
+    })
+    .catch((error) => {
+      console.log(error);
+      return res.json({ error });
     });
+};
 
-    newTest.save()
-        .then(_ => {
-            return res.status(200).json({message: "success"}) 
-        })
-        .catch(error => {
-            console.log(error)
-            return res.json({error})
-        })
-}
+export const updateTest = async (req, res) => {
+  return res.json({ message: "Update test" });
+};
 
-export const updateTest = async (req, res)=>{
-    return res.json({message: "Update test"})
-}
-
-export const deleteTest = async (req, res) =>{
-    return res.json({message: "Delete test"})
-}
+export const deleteTest = async (req, res) => {
+  return res.json({ message: "Delete test" });
+};
 
 export default {
-    getTests,
-    getTest
-}
+  getTests,
+  getTest,
+};
--- models/test.js
+++ models/test.js
@@ -1,10 +1,21 @@
-import mongoose from "mongoose"
+import e from "express";
+import mongoose from "mongoose";
 
-const testSchema = new mongoose.Schema({
-    title: {type: String, required: true},
-    description: {type:String}
-}, {timestamps: true})
+const testSchema = new mongoose.Schema(
+  {
+    title: { type: String, required: true },
+    description: { type: String },
+    // user: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
+    status: {
+      type: String,
+      enum: ["public", "private"],
+      required: true,
+      default: "public",
+    },
+  },
+  { timestamps: true }
+);
 
 export const Test = mongoose.models.Test || mongoose.model("Test", testSchema);
 
-export default Test
+export default Test;
