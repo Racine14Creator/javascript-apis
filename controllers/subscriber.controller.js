e5a8f29e6e39237b1978bce0695c46ad232fd5d0
racine14creator <bisimwagrace596@gmail.com>
2025-01-02 07:49:12 +0200

    Subscriber controller init and ready

--- controllers/subscriber.controller.js
+++ controllers/subscriber.controller.js
@@ -1,8 +1,40 @@
+import { z } from "zod";
+
+import Subscriber from "../models/sub.js";
+
+const subSchema = z.object({
+  email: z.string().email("L'address e-mail est invalid"),
+});
 export const getSubs = async (req, res) => {
-  return res.json({ message: "Get subs" });
+  try {
+    const subs = await Subscriber.find();
+    return res.json({ data: subs, message: "All Subs" });
+  } catch (error) {
+    console.log(error);
+    return res.json({ message: error });
+  }
 };
 
 export const postSub = async (req, res) => {
+  const { email } = subSchema.parse(req.body);
+
+  const existingSub = await Subscriber.findOne({ email });
+
+  if (existingSub) {
+    return res.json({ message: "Cet e-mail est déjà utilisé" }).status(400);
+  }
+
+  const newSub = new Subscriber({
+    email,
+  });
+  newSub
+    .save()
+    .then((_) => {
+      return res.json({ message: "Votre e-mail est enregistree", email });
+    })
+    .catch((error) => {
+      return res.json({ message: error });
+    });
   return res.json({ message: "Get Post Sub" });
 };
 
